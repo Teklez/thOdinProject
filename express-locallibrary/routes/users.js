30770919fef49a0313c414dbef4c68b43801d40a
zemenu <zemenumekiria@gmail.com>
2024-07-15 21:44:42 +0300

    mongoose setted up

--- express-locallibrary/routes/users.js
+++ express-locallibrary/routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/cool', (req, res) => {
+  res.send("you are so cool!");
+})
 
 module.exports = router;
