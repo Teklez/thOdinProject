02dd7ffe2cd1ced4c4ccbdd2c8a82a9361dadd98
zemenu <zemenumekiria@gmail.com>
2024-07-17 22:45:26 +0300

    controller for rotes add

--- express-locallibrary/app.js
+++ express-locallibrary/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
-
+require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const mongoDB = process.env.db;
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/use.rs', usersRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
