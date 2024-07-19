cce0f720225456b313a32f73e487df22a194605a
zemenu <zemenumekiria@gmail.com>
2024-07-19 17:34:22 +0300

    view and controller added for bookinstance detail

--- express-locallibrary/controllers/authorController.js
+++ express-locallibrary/controllers/authorController.js
@@ -1,4 +1,5 @@
 const Author = require("../models/author");
+const Book = require("../models/book");
 const asyncHandler = require('express-async-handler');
 
 
@@ -11,7 +12,24 @@ exports.author_list = asyncHandler(async(req, res, next) => {
 
 // Display detail page for a specific Author.
 exports.author_detail = asyncHandler(async (req, res,next) =>{
-    res.send(`NOT IMPLEMENTED:Author detail:${req.params.id}`);
+   // Get details of author and all their books (in parallel)
+   const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (author === null) {
+    // No resutls
+    const err = new Error("Author not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("author_detail",{
+    title:"Author Detail",
+    author:author,
+    author_books:allBooksByAuthor
+  })
 });
 
 // Display Author crate form on GET
